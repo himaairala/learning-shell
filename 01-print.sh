7d3b2632facfdaa85fd22b5542e16f28dca660a9
Hima <himabindhuairala@gmail.com>
2023-02-03 22:32:02 +0530

    mysql script

--- 01-print.sh
+++ 01-print.sh
@@ -6,4 +6,4 @@ echo Hello
 #\e - enable COLmHelloCOL - COL CODE , 31-36 (31-Red ,32 -Green,33 -yellow,34 - blue ,35- magenta, 36 -cyan)
 # 0m - Disable the enabled color
 
-echo -e "\e[31mHello in RED Color\n[0n"
+echo -e "\e[31mHello in RED Color\n[0m"
